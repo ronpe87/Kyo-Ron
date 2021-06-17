bd803ef05346352657b8399c43093b03c554f70c
kk <>
2021-06-17 10:18:50 +0900

    サインアップ後プロフィール作成促す。ログアウト機能実装。

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  def after_sign_in_path_for(resource)
+    if current_user.profile.present?
+      root_path
+    else
+      new_profile_path
+    end
+  end
 end
