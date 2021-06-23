a7e2c1a6f5ddee1e47489046b44272ea46c74fb7
kk <>
2021-06-23 22:46:02 +0900

    profileページ遷移後リロードしないとajax機能しないバグ修正

--- app/javascript/packs/profile.js
+++ app/javascript/packs/profile.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 
 document.addEventListener('DOMContentLoaded', () => {
   $(function () {
-    $('.profilePage_image').on('click', function () {
+    $('.icon').on('click', function () {
       $('#profile-upload').fadeIn();
     });
     $('.avatar_cancel').on('click', function () {
