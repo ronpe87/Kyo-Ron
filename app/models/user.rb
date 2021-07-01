404675b46c1341ec70928ea6b414f06a0017d36a
kk <>
2021-07-01 11:56:23 +0900

    フォロー、アンフォロー機能実装。

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,10 @@ class User < ApplicationRecord
   has_one :profile, dependent: :destroy
   has_many :opinions, dependent: :destroy
   has_many :comments, dependent: :destroy
+  has_many :following_relationships, foreign_key: 'follower_id', class_name: 'Relationship', dependent: :destroy
+  has_many :followings, through: :following_relationships, source: :following
+  has_many :follower_relationships, foreign_key: 'following_id', class_name: 'Relationship', dependent: :destroy
+  has_many :followers, through: :follower_relationships, source: :follower
 
   def display_name
     profile&.name || self.email.split('@').first
@@ -20,11 +24,35 @@ class User < ApplicationRecord
     profile || build_profile
   end
 
+  def follow!(user)
+    user_id = get_user_id(user)
+    following_relationships.create!(following_id: user_id)
+  end
+
+  def unfollow!(user)
+    user_id = get_user_id(user)
+    relation = following_relationships.find_by!(following_id: user_id)
+    relation.destroy!
+  end
+
+  def has_followed?(user)
+    following_relationships.exists?(following_id: user.id)
+  end
+
   def avatar_image
     if profile&.avatar&.attached?
       profile.avatar
     else
       'default-avatar.png'
+    end
+  end
+
+  private
+  def get_user_id(user)
+    if user.is_a?(User) #userクラスがインスタンスだった場合(is_a)
+      user.id
+    else
+      user
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,11 @@ Rails.application.routes.draw do
   resources :opinions do
     resources :comments, only: [:index, :create]
   end
+
+  resources :accounts, only: [:show] do
+    resources :follows, only: [:create]
+    resources :unfollows, only: [:create]
+  end
+
   resource :profile, only: [:show, :edit, :update]
 end
