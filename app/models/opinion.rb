230af9a4a182ce296bf8d8eb09833e8d89d7aac2
kk <>
2021-08-12 23:15:19 +0900

    usernameが表示されなかったので修正。sidekiq設定。

--- app/models/opinion.rb
+++ app/models/opinion.rb
@@ -12,8 +12,4 @@ class Opinion < ApplicationRecord
   def display_created_at
     I18n.l(self.created_at, format: :default)
   end
-
-  def author_name
-    user.display_name
-  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -12,10 +12,6 @@ class User < ApplicationRecord
   has_many :follower_relationships, foreign_key: 'following_id', class_name: 'Relationship', dependent: :destroy
   has_many :followers, through: :follower_relationships, source: :follower
 
-  def display_name
-    profile&.name || self.email.split('@').first
-  end
-
   def has_written?(opinion)
     opinions.exists?(id: opinion.id)
   end
