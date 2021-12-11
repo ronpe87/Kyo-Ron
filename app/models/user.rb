65497e0ec68291b3a0ffdf609b30cc18e7cc736c
kk <150450050@ccalumni.meijo-u.ac.jp>
2021-12-11 21:19:17 +0900

    何故かログアウトできない状態になっていた。routeのdevise_scopeをgetで書き直したところ正常に動いた。

--- app/models/user.rb
+++ app/models/user.rb
@@ -58,6 +58,7 @@ class User < ApplicationRecord
   # 以下を追加
   def self.guest
     find_or_create_by!(email: 'guest@example.com') do |user|
+      user.username = 'ゲスト'
       user.password = SecureRandom.urlsafe_base64
       # user.confirmed_at = Time.now  # Confirmable を使用している場合は必要
       # 例えば name を入力必須としているならば， user.name = "ゲスト" なども必要
--- config/routes.rb
+++ config/routes.rb
@@ -25,6 +25,7 @@ Rails.application.routes.draw do
   resource :profile, only: [:show, :edit, :update]
 
   devise_scope :user do
-    post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
+    get 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
+    get '/users/sign_out' => 'devise/sessions#destroy'
   end
 end
