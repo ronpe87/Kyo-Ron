bf93b531a20ad4524d32085014608625a68cba86
kk <150450050@ccalumni.meijo-u.ac.jp>
2021-12-20 16:30:46 +0900

    sidekiq導入

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -30,9 +30,6 @@ document.addEventListener('DOMContentLoaded', () => {
   const theme_data = $('#theme_data').data()
   const theme = theme_data.userTheme
   if (theme == 'dark') {
-  //   $('.label').addClass('dark_font')
-  // }
-  // if ($('body').has('dark_color')){
     $('.header_title').children('a:link').css('color', 'white')
     $('.label').addClass('dark_font')
   }
