70da0e0db10b857f5d80ebe41cad12ab07c40bba
kk <150450050@ccalumni.meijo-u.ac.jp>
2022-01-28 21:39:10 +0900

    他のvueファイルを読み込めるか検証

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,15 +1,21 @@
 <template>
   <div id="app">
     <p>{{ message }}</p>
+    <Top/>
   </div>
 </template>
 
 <script>
+import Top from "./components/Top";
+
 export default {
   data: function () {
     return {
       message: "Hello Vue!"
     }
+  },
+  components: {
+    Top
   }
 }
 </script>
