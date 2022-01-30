9334e44d8c654cd1d57a64db612747aca8b2f252
kk <150450050@ccalumni.meijo-u.ac.jp>
2022-01-30 22:18:57 +0900

    hamlはvue.jsと相性が悪い？部分的にerbファイルを使いvue実装成功。

--- app/javascript/packs/custom_vue.js
+++ app/javascript/packs/custom_vue.js
@@ -2,16 +2,9 @@ import Vue from 'vue/dist/vue.esm'
 import App from '../app.vue'
 
 document.addEventListener('DOMContentLoaded', () => {
-  const app = new Vue({
-    el: '#hello2',
-    data: {
-      message: "聞こえますか？これはvue.jsなのです。htmlの書き込みではありません"
-    },
-    components: { App }
-  })
 
   new Vue({
-    el: '#app1',
+    el: '#app',
     data: {
       message: 'World'
     },
