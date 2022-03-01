b88d46aa8dd26133d658d29f2cc1c90e9107a1a5
kk <150450050@ccalumni.meijo-u.ac.jp>
2022-03-01 11:44:57 +0900

    test失敗。赤青opinionを入力するようにfactoriesに書いていなかったため。

--- spec/factories/opinions.rb
+++ spec/factories/opinions.rb
@@ -2,5 +2,7 @@ FactoryBot.define do
   factory :opinion do
     title { Faker::Lorem.characters(number: 10) }
     content { Faker::Lorem.characters(number: 300) }
+    red_opinion { Faker::Lorem.characters(number: 30) }
+    blue_opinion { Faker::Lorem.characters(number: 30) }
   end
 end
