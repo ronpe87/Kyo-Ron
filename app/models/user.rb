31b56b874a94b8228f657fa17c57c651e6e262cb
kk <150450050@ccalumni.meijo-u.ac.jp>
2022-03-06 15:49:39 +0900

    コメント画面でゲストのusernameがnullになる。serializerを編集。

--- app/models/user.rb
+++ app/models/user.rb
@@ -65,8 +65,8 @@ class User < ApplicationRecord
 
   # 以下を追加
   def self.guest
-    find_or_create_by!(email: 'guest@example.com') do |user|
-      user.username = 'ゲスト'
+    find_or_create_by!(email: 'guest@example.com', username: 'ゲスト') do |user|
+      # user.username = 'ゲスト'
       user.password = SecureRandom.urlsafe_base64
       # user.confirmed_at = Time.now  # Confirmable を使用している場合は必要
       # 例えば name を入力必須としているならば， user.name = "ゲスト" なども必要
